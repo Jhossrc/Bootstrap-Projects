2b73b2e5a4d678acbf425f19cb897bddb4d0d150
Jhoss <kuro.rc10@gmail.com>
2020-05-20 13:51:59 -0500

    Aplicando Estilos al Contador

--- 5-festival/js/app.js
+++ 5-festival/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
   $("#fecha").countdown("2020/08/23", function (evento) {    
-    $(this).html(evento.strftime('%D Días %H Horas %M Minutos %S Segundos'))
+    $(this).html(evento.strftime('<span>%D</span> Días <span> %H</span> Horas <span> %M</span> Minutos <span> %S</span> Segundos'))
   });
 });
