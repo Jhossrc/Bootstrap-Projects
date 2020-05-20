0f44cc44d6a084a9513855517b8ea3538e36901d
Jhoss <kuro.rc10@gmail.com>
2020-05-20 14:33:27 -0500

    Agregando una animación al hacer scroll en el sitio

--- 5-festival/js/app.js
+++ 5-festival/js/app.js
@@ -1,3 +1,18 @@
+/* Cambiar el fondo del nav al hacer scroll */
+window.addEventListener('scroll', (e) => {
+
+  const header = document.getElementById('navegacion-principal')
+
+  const scroll = window.scrollY;
+  if (scroll > 300) {
+    header.classList.add('bg-success');
+  } else {
+    header.classList.remove('bg-success');
+  }
+})
+
+
+/* JQuery Countdown */
 $(document).ready(function () {
   $("#fecha").countdown("2020/08/23", function (evento) {    
     $(this).html(evento.strftime('<span>%D</span> Días <span> %H</span> Horas <span> %M</span> Minutos <span> %S</span> Segundos'))
