befc25ccc5c8740bc908b499ba4f5064f7c31be8
Jhoss <kuro.rc10@gmail.com>
2020-05-22 11:55:42 -0500

    Agregando Gráficas con Morris JS

--- 7-admin/js/app.js
+++ 7-admin/js/app.js
@@ -15,3 +15,24 @@
     });
   }, false);
 })();
+
+new Morris.Line({
+  // ID of the element in which to draw the chart.
+  element: 'ventas',
+  // Chart data records -- each entry in this array corresponds to a point on
+  // the chart.
+  data: [
+    { year: '2015', value: 20 },
+    { year: '2016', value: 10 },
+    { year: '2017', value: 5 },
+    { year: '2018', value: 5 },
+    { year: '2019', value: 20 }
+  ],
+  // The name of the data record attribute that contains x-values.
+  xkey: 'year',
+  // A list of names of data record attributes that contain y-values.
+  ykeys: ['value'],
+  // Labels for the ykeys -- will be displayed when you hover over the
+  // chart.
+  labels: ['Value']
+});
